93a402cc6d7b51b4bf71dab24042acac4a38ebda
Rafaela Dubeux Godoy <rdg@cesar.school>
2026-09-26 12:16:41 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -137,8 +137,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+
